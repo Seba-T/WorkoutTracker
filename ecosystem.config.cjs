006502b25589bad9f4c8c644756b653f658301be
Sebastiano Tocci <seba.tocci@gmail.com>
2022-11-18 16:06:39 +0100

    fixed app script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-  apps : [{
-    script: 'dist/index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+  apps: [
+    {
+      script: "dist/index.js",
+      watch: ".",
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'pnpm install && pm2 reload ecosystem.config.cjs --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "pnpm install && pm2 reload ecosystem.config.cjs --env production",
+      "pre-setup": "",
+    },
+  },
 };
